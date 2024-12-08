1f956ca6b84015d85bbd5da860eca4a348969ff7
Isaac <bennisaac24@gmail.com>
2024-12-08 19:16:29 +0300

    completed array lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Write your solution here!
+//defining cats array
+
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(){
+    let newcat = cats.push("Ralph");
+    return newcat;
+}
+
+function destructivelyPrependCat(){
+    let newcat = cats.unshift("Bob");
+    return newcat;
+}
+
+function destructivelyRemoveLastCat(){
+    let newcat = cats.pop();
+    return newcat;
+}
+
+
+function destructivelyRemoveFirstCat(){
+    let newcat = cats.shift();
+    return newcat;
+}
+
+function appendCat(){
+    let newcat = [...cats.slice(0, 3), "Broom"];
+    return newcat;
+}
+
+function prependCat(){
+    let newcat = ["Arnold", ...cats.slice()];
+    return newcat;
+}
+
+function removeLastCat(){
+    let newcat = [...cats.slice(0, 2)];
+    return newcat;
+}
+
+function removeFirstCat(){
+    let newcat = [...cats.slice(-2)];
+    return newcat;
+}
